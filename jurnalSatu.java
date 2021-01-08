85e866ad7fbb5d7f5a8d28efeec3316e19f850e3
davidsryansa <71364982+davidsryansa@users.noreply.github.com>
2021-01-07 16:04:06 -0800

    Add files via upload

---
+++ jurnalSatu.java
@@ -0,0 +1,7 @@
+public class jurnalSatu{
+	public static void main(String[] args) {
+        System.out.println("Nama	: Davids Ryansa Mamisala");
+        System.out.println("Kelas	: D3RPLA 43-03");
+        System.out.println("NIM	: 6706190068");
+    }
+}
